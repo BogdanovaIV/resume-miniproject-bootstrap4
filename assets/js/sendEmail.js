c34e081603c6d7c6157b623dfae0160baa0a02f5
Irina Bogdanova <irinabogdanova261@gmail.com>
2024-08-30 15:43:55 +0000

    Implememnt emailjs integration to contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    var templateParams = {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    };
+
+    emailjs.send('service_tjyhwst', 'template_eyx6lb9', templateParams).then(
+        (response) => {
+            console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+            console.log('FAILED...', error);
+        },
+    );
+    return false;  // To block from loading a new page
+}
